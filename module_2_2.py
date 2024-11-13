fdc75c52b4be5da9e10eecd335458d2eb7072100
HooryG <hooryegor@gmail.com>
2024-11-13 15:50:58 +0900

    M2 13/11/2024

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+first = int(input('In: '))
+second = int(input('In: '))
+third = int(input('In: '))
+if  first == second == third:
+     print('3')
+elif first == second or second == third or third == first :
+     print('2')
+else:
+    print('0')
+
+
+
