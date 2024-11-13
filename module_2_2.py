cffab542f6edd1f55e2172ecac1dd13940cab21c
HooryG <hooryegor@gmail.com>
2024-11-13 16:20:18 +0900

    M2 13/11/2024

--- module_2_2.py
+++ module_2_2.py
@@ -1,29 +1,27 @@
 first = int(input('In: '))
 second = int(input('In: '))
 third = int(input('In: '))
-if  first == second == third:
-     print('3')
-elif first == second or second == third or third == first :
-     print('2')
+if third == first == second:
+    print('3')
+elif first == second or second == third or third == first:
+    print('2')
 else:
     print('0')
 first = int(input('In: '))
 second = int(input('In: '))
 third = int(input('In: '))
-if  first == second == third:
-     print('3')
-elif first == second or second == third or third == first :
-     print('2')
+if first == second == third:
+    print('3')
+elif first == second or second == third or third == first:
+    print('2')
 else:
     print('0')
 first = int(input('In: '))
 second = int(input('In: '))
 third = int(input('In: '))
-if  first == second == third:
-     print('3')
-elif first == second or second == third or third == first :
-     print('2')
+if first == second == third:
+    print('3')
+elif first == second or second == third or third == first:
+    print('2')
 else:
     print('0')
-
-
